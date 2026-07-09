d1d2feb62c43311f8acfdd8353d126903a2ab7a7
Ntwali Djibril <your_email@example.com>
2026-07-09 10:18:06 +0200

    reddit api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python:api_advanced:v1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
